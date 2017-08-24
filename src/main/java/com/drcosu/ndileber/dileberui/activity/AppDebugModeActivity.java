43dd6a0830ea30a64638864aabfe30659339b970
wangerdan <297165331@qq.com>
2017-08-24 14:55:46 +0800

    添加新方法

--- src/main/java/com/drcosu/ndileber/dileberui/activity/AppDebugModeActivity.java
+++ src/main/java/com/drcosu/ndileber/dileberui/activity/AppDebugModeActivity.java
@@ -3,19 +3,29 @@ package com.drcosu.ndileber.dileberui.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.DrawableRes;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.drcosu.ndileber.app.ActivityManager;
+import com.drcosu.ndileber.app.BaseConfiger;
+import com.drcosu.ndileber.app.FrameContants;
 import com.drcosu.ndileber.dileberui.R;
 import com.drcosu.ndileber.mvp.acivity.BaseActivity;
+import com.drcosu.ndileber.tools.HPref;
+import com.drcosu.ndileber.tools.UUi;
+import com.drcosu.ndileber.utils.LauncherManager;
 import com.drcosu.ndileber.utils.UToolBar;
 
 /**
  * Created by congtaowang 2016/10/29.
  */
 
-public class AppDebugModeActivity extends BaseActivity {
+public class AppDebugModeActivity extends BaseActivity implements View.OnClickListener{
 
     static final String TAG = AppDebugModeActivity.class.getSimpleName();
 
@@ -24,10 +34,12 @@ public class AppDebugModeActivity extends BaseActivity {
 
 
     public static void start(Context context, String name, @DrawableRes int icon){
-        Intent it = new Intent(context,AppDebugModeActivity.class);
-        it.putExtra(NAME,name);
-        it.putExtra(ICON,icon);
-        context.startActivity(it);
+        if(BaseConfiger.BUG_STATIC){
+            Intent it = new Intent(context,AppDebugModeActivity.class);
+            it.putExtra(NAME,name);
+            it.putExtra(ICON,icon);
+            LauncherManager.launcher.launch(context,it);
+        }
     }
 
     @Override
@@ -57,5 +69,24 @@ public class AppDebugModeActivity extends BaseActivity {
         ImageView debug_icon2 = findView(R.id.debug_icon2);
         debug_icon1.setImageResource(it.getIntExtra(ICON,0));
         debug_icon2.setImageResource(it.getIntExtra(ICON,0));
+        setOnClickListener(this,R.id.test,R.id.onLine);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.test) {
+            HPref.getInstance().put(FrameContants.SYSTEM_PREFERANCE,FrameContants.SYSTEM_PREFERANCE_DEBUG_TYPE,FrameContants.SYSTEM_PREFERANCE_DEBUG_TEST);
+        } else if (i == R.id.onLine) {
+            HPref.getInstance().put(FrameContants.SYSTEM_PREFERANCE,FrameContants.SYSTEM_PREFERANCE_DEBUG_TYPE,FrameContants.SYSTEM_PREFERANCE_DEBUG_ONLINE);
+        }
+        UUi.toast(this,"配置完成", Toast.LENGTH_SHORT);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ActivityManager.getInstance().finishAllActivity();
+                System.exit(1);
+            }
+        }, 1000);
     }
 }
